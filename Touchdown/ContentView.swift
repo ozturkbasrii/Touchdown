e3930b360f822782499b91fbe06b3197a83f3143
ozturkbasrii <ozturkbasrii@gmail.com>
2022-10-03 09:54:37 +0300

    Initial Commit

---
+++ Touchdown/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Touchdown
+//
+//  Created by Hasan Basri Öztürk on 3.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Hasan Basri Öztürk on 3.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
